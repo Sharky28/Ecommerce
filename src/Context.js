40aa43caccb6f3aaaa7b410a85e85e217e439750
SharkyUgas <sharmarke.ugas@outlook.com>
2019-07-11 14:09:51 +0100

    added product list, product along with styling

--- src/Context.js
+++ src/Context.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react'
-
-const ProductContext =React.createContext();
+import { storeProducts, detailProduct } from './data';
+const ProductContext = React.createContext();
 //Provider
 //Consumer
 
 class ProductProvider extends Component {
+    state = {
+        products: storeProducts,
+        detailProduct: detailProduct
+    }
+    handleDetail = () => {
+        console.log('hello from detail');
+    }
+
+    addToCart = () => {
+        console.log('hello from add to cart');
+    }
+
     render() {
         return (
-          <ProductContext.Provider value ="hello from context">
-              {this.props.children}
-          </ProductContext.Provider>
+            <ProductContext.Provider value={{
+                ...this.state,
+                handleDetail: this.handleDetail,
+                addToCart: this.addToCart
+            }}>
+                {this.props.children}
+            </ProductContext.Provider>
         )
     }
 }
 
 const ProductConsumer = ProductContext.Consumer;
 
-export{ProductProvider,ProductConsumer};
+export { ProductProvider, ProductConsumer };
